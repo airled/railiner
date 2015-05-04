86cfca1c7ae9bda606cf3e73ea2f39da1937dfb6
airled <airled@mail.ru>
2015-05-04 16:36:03 +0300

    last

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ Rails.application.routes.draw do
 
   get 'categories/', to: 'categories#index_all'
   get 'products/', to: 'products#index_all'
+  
   resources :groups, only: [:index] do
     resources :categories, only: [:index] do
       resources :products, only: [:index]
     end
   end
-#   resources :categories do
-#     resources :products do     
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
