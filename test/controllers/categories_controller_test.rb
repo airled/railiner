b780a692b5a1ad778dcb176078152b52371e7e2a
airled <airled@mail.ru>
2015-05-17 18:05:03 +0300

    add some tests

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class CategoriesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should get index_all" do
+    get :index_all
+    assert_response :success
+    assert_not_nil assigns(:categories)
+  end
+
+  test 'index_all should render correct template' do
+    get :index_all
+    assert_template :index_all
+  end
+
+  test "should get index" do
+  	group_id = Group.first.id
+    get :index, group_id: group_id
+    assert_response :success
+    assert_not_nil assigns(:categories)
+  end
+
+  test 'index should render correct template' do
+    group_id = Group.first.id
+    get :index, group_id: group_id
+    assert_template :index
+  end
+
+  test "should get show" do
+  	id = Category.first.id
+    get :show, id: id
+    assert_response :success
+    assert_not_nil assigns(:products)
+  end
+
+  test 'show should render correct template' do
+    id = Category.first.id
+    get :show, id: id
+    assert_template :show
+  end
+  
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
+
+  test "should get index_all" do
+    get :index_all
+    assert_response :success
+    assert_not_nil assigns(:products)
+  end
+
+  test 'index_all should render correct template' do
+    get :index_all
+    assert_template :index_all
+  end
+
+  # test "should get index" do
+  # 	# group_id = Group.first.id
+  # 	group = Group.first
+  # 	group_id = group.id
+  # 	category_id = group.categories.first.id
+
+  #   get :index, group_id: group_id, catewgory_id: categories_id
+  #   assert_response :success
+  #   assert_not_nil assigns(:categories)
   # end
+
 end
