971584c9e949be43d6f87e1a3f589f345d7a2d79
airled <airled@mail.ru>
2015-05-31 21:12:57 +0300

    thinking

--- lib/worker.rb
+++ lib/worker.rb
@@ -12,13 +12,13 @@ class Worker
       category_url_for_worker = 'https://catalog.api.onliner.by/search/' + category_url.sub(URL,'').delete('/') + "?page=#{page_number}"
       page = JSON.parse(Curl.get(category_url_for_worker).body_str)
       page_number = if page['products'].any?
-        page['products'].map do |x|
-          url = x['html_url']
-          name = x['full_name']
-          image_url = x['images']['header']
+        page['products'].map do |product|
+          url = product['html_url']
+          name = product['full_name']
+          image_url = product['images']['header']
           @hash_of_products_from_web << {url: url, name: name, image_url: image_url}
         end
-        page_number +=1
+        page_number += 1
       end || false
     end
   end
@@ -26,13 +26,12 @@ class Worker
   def find_category_db(category_url)
     category = Category.find_by(url: category_url)
     @hash_of_products_from_db = category.products.map { |product| {url: product.url, name: product.name, image_url: product.image_url} }
-    p @hash_of_products_from_db
+    p @hash_of_products_from_db.size
   end
 
   def compare
-    # @hash_of_products_from_web.map do |product_hash|
-
-    # end
+    @hash_of_products_from_web.map do |web_product_hash|
+        
   end
 
   def create_product
@@ -48,3 +47,5 @@ end
 
 # Worker.new.get_category_products('http://catalog.onliner.by/mobile/');
 # A="https://catalog.api.onliner.by/search/mobile?birthday[from]=2015&mp_ruggedcase=false&is_actual=1"
+
+# select count(*) from categories_products join categories on categories.id=categories_products.category_id join products on products.id=categories_products.product_id where categories.name='mobile';
