8844cf972c7fc5c749d0e9dc612a6aa0db30fcc9
airled <airled@mail.ru>
2015-06-04 10:21:42 +0300

    modify added

--- lib/worker.rb
+++ lib/worker.rb
@@ -4,11 +4,17 @@ require 'curb'
 class Worker
 
   URL = 'http://catalog.onliner.by'
+  url = 'http://catalog.onliner.by/mobile/'
 
   def run(url)
     get_category_products(url)
     find_category_db(url)
     compare_web_to_db
+    if @modified.size != 0
+      puts "Do you like to modify your records? Y/n"
+      answer = STDIN.gets.chomp
+      update if answer == ('Y' || 'y')
+    end
   end
 
   def get_category_products(category_url)
@@ -37,24 +43,24 @@ class Worker
   end
 
   def compare_web_to_db
-    new = []
-    modified = []
+    @new = []
+    @modified = []
     @products_from_web.each do |web_product|
       mismatch_count = 0;
       @products_from_db.each do |db_product|
         if web_product[:url] == db_product[:url] && (web_product[:name] != db_product[:name] || web_product[:image_url] != db_product[:image_url])
-          modified << web_product
+          @modified << web_product
         end
         if web_product[:url] != db_product[:url]
           mismatch_count += 1 
         end
       end
       if mismatch_count == @products_from_db.size
-        new << web_product
+        @new << web_product
       end
     end
-    puts "Existing records to modify: #{modified.size}"
-    puts "New records to add: #{new.size}"
+    puts "Existing records to modify: #{@modified.size}"
+    puts "New records to add: #{@new.size}"
   end
 
   def compare_db_to_web
@@ -64,6 +70,12 @@ class Worker
   end
 
   def update
+    puts 'Modifying database records...'
+    @modified.map do |web_hash|
+      product = Product.find_by(url: web_hash[:url])
+      product.update(web_hash)
+    end
+    puts 'Modified'
   end
 
   def destroy
@@ -71,7 +83,6 @@ class Worker
   
 end
 
-# url = 'http://catalog.onliner.by/mobile/'
 # Worker.new.find_from_db(url)
 
 # Worker.new.get_category_products('http://catalog.onliner.by/mobile/');
