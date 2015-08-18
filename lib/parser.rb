13659b7b52899aa2858dd22b9583dab481d90ea0
airled <airled@mail.ru>
2015-08-18 20:32:56 +0300

    new parser - begin (like a batman)

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,109 +1,37 @@
-#Onliner's catalog parser. Fetches groups of categories, categories
-#and products of each category and inserts them all into the 
-#appropriate MySQL table.
 require 'nokogiri'
 require 'open-uri'
-require 'ruby-progressbar'
-require 'singleton'
+require 'json'
 
 class Parser
 
-
-# require 'json'
-
-# file = File.open('./2.rb','r')
-# huita = file.read
-# p JSON.parse(huita)
-# file.close
-#   include Singleton
-
-  #Onliner's catalog URL
   URL = 'http://catalog.onliner.by'
 
-  #parser's work script
   def run
-    start_time = Time.new
-    #fetching HTML code
-    puts 'Fetching start page HTML...'
-    html = Nokogiri::HTML(open(URL))
-    puts 'Fetched.'
-    #fetching groups and categories root nodes
-    groups = html.xpath("//h1[@class='cm__h1']")
-    categories_blocks = html.xpath("//ul[@class='b-catalogitems']")
-    categories_amount = categories_blocks.xpath("./li/div[@class='i']").size
-    puts 'Parsing...'
-    progress_options = {title: ">Progress", starting_at: 0, total: categories_amount}
-    progress_bar = ProgressBar.create(progress_options)
-    #matching products to their categories and categories to their groups
-    groups.zip(categories_blocks).map do |group_node, categories_block|
-      group = create_group(group_node)
-      categories_block.xpath("./li/div[@class='i']").map do |category_node|
-        category = group.categories.create(category_parameters(category_node))
-        create_category_products(category,category_node)
-        progress_bar.increment
+    html = get_html(URL)
+    script_tag = html.xpath('//div[@class="g-middle"]/div[@class="g-middle-i"]/script[1]').text.strip
+    hash = get_hash(script_tag)
+    file = File.open('1.txt','w')
+
+    hash.map do |large_category|
+      large_category[1]['groups'].map do |group|
+        group['links'].map do |link|
+          file << group['title'] + '|||' + link['title'] << "\n"
+        end
       end
     end
-    stop_time = Time.new
-    progress_bar.finish
-    #echo result information
-    results(stop_time,start_time)
+
+    file.close
+
   end
 
-  private
-
-  #creating groups in Groups table
-  def create_group(group_node)
-    name_ru = group_node.text.delete("0-9")
-    Group.create(name_ru: name_ru, name: '')
+  def get_html(source)
+    Nokogiri::HTML(open(source))
   end
 
-  #fetching hash parameters of one category
-  def category_parameters(category_node)
-    url = category_node.xpath("./a[1]/@href").text
-    name = url.sub(URL,'').delete('/')
-    name_ru = category_node.xpath("./a[last()]").text
-    is_new = category_node.xpath("./a[2]/img[@class='img_new']").any?
-    {name: name, name_ru: name_ru, url: url, is_new: is_new}
+  def get_hash(text)
+    JSON.parse(text.sub('window.categories = ','').chop)
   end
 
- #creating all products of a category
-  def create_category_products(category,category_node)
-    products_page_url = category_node.xpath("./a[1]/@href").text
-    while products_page_url
-      html_product = Nokogiri::HTML(open(products_page_url))
-      html_product.xpath("//tr/td[@class='pdescr']").map do |product_node|
-        category.products.create(product_parameters(product_node))
-      end
-      products_page_url = check_next(html_product)
-    end
-  end
-
-  #fetching hash parameters of one product
-  def product_parameters(product_node)
-    url = URL + product_node.xpath("./strong/a/@href").text
-    name = product_node.xpath("./strong/a").text.strip
-    image_url = product_node.xpath("../td[@class='pimage']/a/img/@src").text
-    {url: url, name: name, image_url: image_url}
-  end
-
-  #checking if there is a next product page in the same category
-  def check_next(products_page)
-    xnext = "//td[@align='right']/strong/a[contains(text(), 'Следующие')]/@href"
-    next_url = products_page.xpath(xnext).text
-    next_products_page_url = if next_url != ''
-      URL + '/' + next_url
-    end || false
-    next_products_page_url
-  end
-  
-  #calculating parsing time and amount of fetched objects
-  def results(stop,start)
-    secs_total = (stop - start).to_i
-    hours = secs_total / 3600
-    mins = (secs_total - hours * 3600) / 60
-    secs = secs_total - hours * 3600 - mins * 60
-    puts "Done in #{hours}:#{mins}:#{secs}"
-    puts "Got: #{Group.count} groups, #{Category.count} categories, #{Product.count} products"
-  end
- 
 end
+
+Parser.new.run
---
+++ lib/parser_old.rb
@@ -0,0 +1,102 @@
+#Onliner's catalog parser. Fetches groups of categories, categories
+#and products of each category and inserts them all into the 
+#appropriate MySQL table.
+require 'nokogiri'
+require 'open-uri'
+require 'ruby-progressbar'
+require 'singleton'
+
+class Parser
+
+  include Singleton
+
+  #Onliner's catalog URL
+  URL = 'http://catalog.onliner.by'
+
+  #parser's work script
+  def run
+    start_time = Time.new
+    #fetching HTML code
+    puts 'Fetching start page HTML...'
+    html = Nokogiri::HTML(open(URL))
+    puts 'Fetched.'
+    #fetching groups and categories root nodes
+    groups = html.xpath("//h1[@class='cm__h1']")
+    categories_blocks = html.xpath("//ul[@class='b-catalogitems']")
+    categories_amount = categories_blocks.xpath("./li/div[@class='i']").size
+    puts 'Parsing...'
+    progress_options = {title: ">Progress", starting_at: 0, total: categories_amount}
+    progress_bar = ProgressBar.create(progress_options)
+    #matching products to their categories and categories to their groups
+    groups.zip(categories_blocks).map do |group_node, categories_block|
+      group = create_group(group_node)
+      categories_block.xpath("./li/div[@class='i']").map do |category_node|
+        category = group.categories.create(category_parameters(category_node))
+        create_category_products(category,category_node)
+        progress_bar.increment
+      end
+    end
+    stop_time = Time.new
+    progress_bar.finish
+    #echo result information
+    results(stop_time,start_time)
+  end
+
+  private
+
+  #creating groups in Groups table
+  def create_group(group_node)
+    name_ru = group_node.text.delete("0-9")
+    Group.create(name_ru: name_ru, name: '')
+  end
+
+  #fetching hash parameters of one category
+  def category_parameters(category_node)
+    url = category_node.xpath("./a[1]/@href").text
+    name = url.sub(URL,'').delete('/')
+    name_ru = category_node.xpath("./a[last()]").text
+    is_new = category_node.xpath("./a[2]/img[@class='img_new']").any?
+    {name: name, name_ru: name_ru, url: url, is_new: is_new}
+  end
+
+ #creating all products of a category
+  def create_category_products(category,category_node)
+    products_page_url = category_node.xpath("./a[1]/@href").text
+    while products_page_url
+      html_product = Nokogiri::HTML(open(products_page_url))
+      html_product.xpath("//tr/td[@class='pdescr']").map do |product_node|
+        category.products.create(product_parameters(product_node))
+      end
+      products_page_url = check_next(html_product)
+    end
+  end
+
+  #fetching hash parameters of one product
+  def product_parameters(product_node)
+    url = URL + product_node.xpath("./strong/a/@href").text
+    name = product_node.xpath("./strong/a").text.strip
+    image_url = product_node.xpath("../td[@class='pimage']/a/img/@src").text
+    {url: url, name: name, image_url: image_url}
+  end
+
+  #checking if there is a next product page in the same category
+  def check_next(products_page)
+    xnext = "//td[@align='right']/strong/a[contains(text(), 'Следующие')]/@href"
+    next_url = products_page.xpath(xnext).text
+    next_products_page_url = if next_url != ''
+      URL + '/' + next_url
+    end || false
+    next_products_page_url
+  end
+  
+  #calculating parsing time and amount of fetched objects
+  def results(stop,start)
+    secs_total = (stop - start).to_i
+    hours = secs_total / 3600
+    mins = (secs_total - hours * 3600) / 60
+    secs = secs_total - hours * 3600 - mins * 60
+    puts "Done in #{hours}:#{mins}:#{secs}"
+    puts "Got: #{Group.count} groups, #{Category.count} categories, #{Product.count} products"
+  end
+ 
+end
