53b5514288dd04edd2f25d0ef957aaa394869a02
airled <airled@mail.ru>
2015-08-21 15:22:05 +0300

    fixed

--- lib/parser.rb
+++ lib/parser.rb
@@ -42,12 +42,13 @@ class Parser
   end
 
   def request(url)
+    # Curl.get(url).body_str
     user_agents = ['Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36', 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko', 'Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1250.0 Iron/22.0.2150.0 Safari/537.4', 'Mozilla/5.0 (X11; Linux) KHTML/4.9.1 (like Gecko) Konqueror/4.9', 'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/533.1 (KHTML, like Gecko) Maxthon/3.0.8.2 Safari/533.1', 'Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/534.10 (KHTML, like Gecko) Ubuntu/10.10 Chromium/8.0.552.237 Chrome/8.0.552.237 Safari/534.10', 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:39.0) Gecko/20100101 Firefox/39.0']
-    c = Curl::Easy.new(url) do |http| 
-      http.headers["User-Agent"] = user_agents(user_agents[rand(user_agents.size)])
-      # curl.verbose = true
+    data = Curl::Easy.new(url) do |http| 
+      http.headers["User-Agent"] = user_agents[rand(user_agents.size)]
     end
-    c.perform
+    data.perform
+    data.body_str
   end
 
   def get_hash(text)
