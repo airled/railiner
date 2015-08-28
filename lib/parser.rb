15c2985168559abb4d93dc02ca0bfbf3c3eae8ca
airled <airled@mail.ru>
2015-08-28 11:56:53 +0300

    parser v3.0

--- lib/parser.rb
+++ lib/parser.rb
@@ -8,7 +8,7 @@ class Parser
   URL = 'http://catalog.onliner.by/'
 
   def run
-    start_time = Time.new
+    start_stats
     html = get_html(URL) 
     group_nodes = []
     categories_nodes = []
@@ -17,26 +17,16 @@ class Parser
     group_nodes.zip(categories_nodes).map do |group_node, categories_node|
       db_group = create_group(group_node.text)
       categories_node.xpath('./li/span[@class="catalog-navigation-list__link-inner"]').map do |node|
-        category_name = node.xpath('./a/@href').text.sub(URL,'').split('?').first
+        category_name = node.xpath('./a/@href').text.sub(URL,'').split('/').first.split('?').first
         db_category = create_group_category(db_group, node, category_name)
         products_request_url = 'https://catalog.api.onliner.by/search/' + category_name
         pages_quantity = JSON.parse(curl_request(products_request_url))['page']['last'].to_i
-        puts "...#{category_name}/#{pages_quantity} pages"
-        1.upto(pages_quantity) do |page_number|
-          begin
-            get_products_from_page(products_request_url + '?page=' + page_number.to_s, db_category)
-            sleep(0.5)
-          rescue
-            puts 'Disconnected. Retrying...'
-            sleep(5)
-            redo
-          end
-        end
+        parse_pages(category_name, pages_quantity, products_request_url, db_category)
         sleep(5)
       end
     end
-    stop_time = Time.new
-    results(stop_time, start_time)
+    stop_stats
+    results(stop_stats, start_stats)
   end #def
 
   def get_html(source)
@@ -54,15 +44,6 @@ class Parser
     group.categories.create(url: url, name_ru: name_ru, name: name)
   end
   
-  def curl_request(url)
-    user_agents = ['Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:39.0) Gecko/20100101 Firefox/39.0', 'Mozilla/5.0 (Windows NT 6.1; rv:35.0) Gecko/20100101 Firefox/35.0', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36']
-    data = Curl::Easy.new(url) do |http| 
-      http.headers["User-Agent"] = user_agents[rand(user_agents.size)]
-    end
-    data.perform
-    data.body_str
-  end
-  
   def get_products_from_page(url, category)
     json = curl_request(url)
     JSON.parse(json)['products'].map do |product|
@@ -72,14 +53,48 @@ class Parser
       category.products.create(name: name, url: url, image_url: image_url)
     end
   end
+
+  def curl_request(url)
+    user_agents = ['Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:39.0) Gecko/20100101 Firefox/39.0', 'Mozilla/5.0 (Windows NT 6.1; rv:35.0) Gecko/20100101 Firefox/35.0', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36']
+    data = Curl::Easy.new(url) do |http| 
+      http.headers["User-Agent"] = user_agents[rand(user_agents.size)]
+    end
+    data.perform
+    data.body_str
+  end
   
+  def parse_pages(category_name, quantity, url, db_category)
+    puts "...#{category_name}/#{quantity} pages"
+    1.upto(quantity) do |page_number|
+      begin
+        get_products_from_page(url + '?page=' + page_number.to_s, db_category)
+        sleep(0.5)
+      rescue
+        puts 'Disconnected. Retrying...'
+        sleep(5)
+        redo
+      end
+    end
+  end
+
+  def start_stats
+    {time: Time.new, groups: Group.count, categories: Category.count, products: Product.count}
+  end
+
+  def stop_stats
+    {time: Time.new, groups: Group.count, categories: Category.count, products: Product.count}
+  end
+
   def results(stop, start)
-    secs_total = (stop - start).to_i
+    secs_total = (stop[:time] - start[:time]).to_i
     hours = secs_total / 3600
     mins = (secs_total - hours * 3600) / 60
     secs = secs_total - hours * 3600 - mins * 60
+    groups_delta = stop[:groups] - start[:groups]
+    categories_delta = stop[:categories] - start[:categories]
+    products_delta = stop[:products] - start[:products]
     puts "Done in #{hours}:#{mins}:#{secs}"
-    # puts "Got: #{Group.count} groups, #{Category.count} categories, #{Product.count} products"
+    puts "Got: #{groups_delta} groups, #{categories_delta} categories, #{products_delta} products"
   end
 
 end
