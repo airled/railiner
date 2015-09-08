44012536501e733d86d327b4d1ed8597464d1ba7
airled <airled@mail.ru>
2015-09-08 18:07:27 +0300

    unicorn preparations

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,24 +1,24 @@
 worker_processes 2
 
-working_directory '/home/air/railiner'
+working_directory '/home/onliner/current'
 
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
 # listen "/home/air/railiner/socket/.unicorn.sock", :backlog => 64
-listen 8080, :tcp_nopush => true
+listen 3000, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
 
 # feel free to point this anywhere accessible on the filesystem
-pid "/home/air/railiner/tmp/pids/unicorn.pid"
+pid "/home/onliner/current/tmp/pids/unicorn.pid"
 
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
 # so prevent them from going to /dev/null when daemonized here:
-stderr_path "/home/air/railiner/log/unicorn.stderr.log"
-stdout_path "/home/air/railiner/log/unicorn.stdout.log"
+stderr_path "/home/onliner/current/log/unicorn.stderr.log"
+stdout_path "/home/onliner/current/log/unicorn.stdout.log"
 
 # combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
