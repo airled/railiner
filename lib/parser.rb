6bd4ecab33500b2f004c90e966c728ba67bef9a5
airled <airled@mail.ru>
2015-09-14 10:11:38 +0300

    views with description

--- lib/parser.rb
+++ lib/parser.rb
@@ -80,8 +80,8 @@ class Parser
       name = product['full_name']
       url = product['html_url']
       image_url = product['images']['icon']
-      min_price = product['prices']['min'].reverse.scan(/\d{1,3}/).join(' ').reverse
-      min_price = product['prices']['max'].reverse.scan(/\d{1,3}/).join(' ').reverse
+      min_price = product['prices']['min'].to_s.reverse.scan(/\d{1,3}/).join(' ').reverse
+      max_price = product['prices']['max'].to_s.reverse.scan(/\d{1,3}/).join(' ').reverse
       description = product['description']
       category.products.create(name: name, url: url, image_url: image_url, max_price: max_price, min_price: min_price, description: description)
     end
