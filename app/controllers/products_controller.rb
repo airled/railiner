8eb13c68752d58319ed41f0de920d2e8918c11a1
airled <airled@mail.ru>
2015-09-16 13:13:19 +0300

    form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,5 +3,10 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all.page(params[:page])
   end
+
+  def find
+    @product = Product.find_by(:name => params[:form][:product])
+    redirect_to @product.url
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get 'info', to: 'info#stats'
+  post 'find', to: 'products#find'
   get 'categories/', to: 'categories#index_all'
   get 'products/(page/:page)', to: 'products#index', as: :products
   resources :categories, only: [:show], param: :category_name
