8beb4f6e280cada7b70dec56013a018e3d977eb8
airled <airled@mail.ru>
2015-09-17 13:31:13 +0300

    finding pagination/find via get

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,9 +7,9 @@ class ProductsController < ApplicationController
   def find
     request = params[:form][:product]
     if params[:form][:out] == '0'
-      @products = Product.where("name like ?", "%#{request}%").where("min_price !=?", "N/A")
+      @products = Product.where("name like ?", "%#{request}%").where("min_price !=?", "N/A").page(params[:page])
     else
-      @products = Product.where("name like ?", "%#{request}%")
+      @products = Product.where("name like ?", "%#{request}%").page(params[:page])
     end
   end
   
