3558e428e72760409de4e3ea070903a45039ae31
airled <airled@mail.ru>
2015-09-18 15:44:50 +0300

    no ssl verification

--- lib/parser.rb
+++ lib/parser.rb
@@ -50,6 +50,7 @@ class Parser
   def curl_request(url)
     user_agents = ['Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:39.0) Gecko/20100101 Firefox/39.0', 'Mozilla/5.0 (Windows NT 6.1; rv:35.0) Gecko/20100101 Firefox/35.0', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36']
     data = Curl.get(url) do |http| 
+      http.ssl_verify_peer = false
       http.headers["User-Agent"] = user_agents[rand(user_agents.size)]
     end
     data.perform
