f43668f94cbdb534b15bb20119677a09507bf68e
airled <airled@mail.ru>
2015-10-13 13:15:45 +0300

    better xpath

--- lib/parser.rb
+++ lib/parser.rb
@@ -102,10 +102,10 @@ class Parser
     loop do
       html = get_html(url)
       if (!html.text.include?('503 Service Temporarily Unavailable'))
-        rows = html.xpath('//div[@id="region-minsk"]/div[@class="b-offers-list-line-table"]/table[@class="b-offers-list-line-table__table"]/tbody[@class="js-position-wrapper"]/tr//a[@class="js-currency-primary"]')
+        rows = html.xpath('//div[@id="region-minsk"]/div[@class="b-offers-list-line-table"]/table[@class="b-offers-list-line-table__table"]/tbody[@class="js-position-wrapper"]/tr')
         rows.map do |row|
-          price = row.text
-          seller_id = row.xpath('./@href').text.sub('http://','').split('.').first
+          price = row.xpath('./td[1]//a').text.strip
+          seller_id = row.xpath('./@data-shop').text.strip
           product.costs.create(seller_id: seller_id, price: price)
         end
         break
