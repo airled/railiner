a2cc3709a745499c0d67f20a88101f8fd3e344cd
Vladimir <airled@mail.ru>
2015-10-15 20:32:15 +0300

    Update worker.rb

--- app/workers/worker.rb
+++ app/workers/worker.rb
@@ -4,7 +4,7 @@ class Worker
 
   def perform(product_url, product)
     loop do
-      html = Nokogiri::HTML(Curl.get(product_url + '/prices#region=minsk&currency=byr'))
+      html = Nokogiri::HTML(Curl.get(product_url + '/prices#region=minsk&currency=byr').body)
       if (!html.text.include?('503 Service Temporarily Unavailable'))
         rows = html.xpath('//div[@id="region-minsk"]/div[@class="b-offers-list-line-table"]/table[@class="b-offers-list-line-table__table"]/tbody[@class="js-position-wrapper"]/tr')
         rows.map do |row|
