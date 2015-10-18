0cc26acf9f01e1a00202ab3a2ac6b58cbb24e0bf
Vladimir <airled@mail.ru>
2015-10-18 11:11:12 +0300

    Update sellers_filler.rb

--- lib/sellers_filler.rb
+++ lib/sellers_filler.rb
@@ -5,7 +5,7 @@ class Filler
 
   def self.run
     sellers_ids = Cost.select(:seller_id).distinct.map(&:seller_id)
-    sellers_ids.map do |seller_id|
+    sellers_ids.sort.map do |seller_id|
       print "\rcurrent id: #{seller_id}"
       html = Nokogiri::HTML(Curl.get("#{seller_id}.shop.onliner.by").body)
       name = html.xpath('//h1[@class="sells-title"]').text
