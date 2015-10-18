98878c733200d8ac661b9ced9cb3765e25cbfbc2
airled <airled@mail.ru>
2015-10-18 16:13:28 +0300

    fix

--- lib/sellers_filler.rb
+++ lib/sellers_filler.rb
@@ -7,7 +7,7 @@ class Filler
     sellers_ids = Cost.select(:seller_id).distinct.map(&:seller_id)
     amount = sellers_ids.size
     sellers_ids.sort.map.with_index do |seller_id, index|
-      print "\rcurrent id: #{seller_id} - #{index}/#{amount}"
+      print "\rcurrent id: #{seller_id} - #{index + 1}/#{amount}"
       loop do
         html = Nokogiri::HTML(Curl.get("#{seller_id}.shop.onliner.by").body)
         if (!html.text.include?('503 Service Temporarily Unavailable'))
@@ -17,6 +17,7 @@ class Filler
         end #if
       end #loop
     end #map
+    puts "\nSellers filled."
   end #def
 
 end
