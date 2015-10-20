6a52e2345394b2f177354f3fd29301bea4f80df7
airled <airled@mail.ru>
2015-10-20 09:12:35 +0300

    filler update

--- lib/sellers_filler.rb
+++ lib/sellers_filler.rb
@@ -7,17 +7,17 @@ class Filler
     sellers_ids = Cost.select(:seller_id).distinct.map(&:seller_id)
     amount = sellers_ids.size
     sellers_ids.sort.map.with_index do |seller_id, index|
-      print "\rcurrent id: #{seller_id} - #{index + 1}/#{amount}"
+      print "\rFilled: #{(100 * (index + 1)) / amount}%"
       loop do
         html = Nokogiri::HTML(Curl.get("#{seller_id}.shop.onliner.by").body)
-        if (!html.text.include?('503 Service Temporarily Unavailable'))
+        unless html.text.include?('503 Service Temporarily Unavailable')
           name = html.xpath('//h1[@class="sells-title"]').text.strip
           Seller.create(id: seller_id, name: name) if name != ''
           break
-        end #if
+        end #unless
       end #loop
     end #map
-    puts "\nSellers filled."
+    puts "\r#{Seller.count} sellers filled."
   end #def
 
 end
