cc9fbcd1282ec99661cbadc8717aa538c4c5260c
airled <airled@mail.ru>
2015-10-22 13:24:46 +0300

    -

--- Rakefile
+++ Rakefile
@@ -1,6 +1,5 @@
 # Add your own tasks in files placed in lib/tasks ending in .rake,
 # for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
-
 require File.expand_path('../config/application', __FILE__)
 Rails.application.load_tasks
 
@@ -10,7 +9,7 @@ task :parse => :environment do
 end
 
 task :reparse => :environment do
-  system('rake db:rollback STEP=8 && rake db:migrate && rake parse')
+  system('rake reset && rake parse')
 end
 
 task :fill => :environment do
@@ -36,16 +35,29 @@ task :skk do
   puts 'Sidekiq killed.'
 end
 
-namespace :app do
+namespace :l do
 
-  task :run do
+  task :start do
     system('bundle exec unicorn -c config/unicorn.rb -E production -D')
+    puts 'Unicorn started.'
   end
 
   task :stop do
     pid = File.open('/home/onliner/current/tmp/pids/unicorn.pid') { |f| f.read }
     system("kill #{pid.strip}")
     puts 'Unicorn killed.'
+  end
+
+end
+
+namespace :r do
+
+  task :start do
+    system('bundle exec cap production app:start')
+  end
+
+  task :stop do
+    system('bundle exec cap production app:stop')
   end
 
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,54 +1,39 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
-
 set :default_env, { 'PATH' => '$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH' }
-# set :default_environment, { 'PATH' => '$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH' }
-set :default_shell, '/bin/zsh --login'
-
+# set :default_shell, '/bin/zsh --login'
 set :application, 'railiner'
 set :repo_url, 'git@github.com:airled/railiner'
-
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/home/onliner'
 set :tmp_dir, '/home/onliner/tmp'
 # Default value for :scm is :git
 # set :scm, :git
-
 # Default value for :format is :pretty
 # set :format, :pretty
-
 # Default value for :log_level is :debug
 # set :log_level, :debug
-
 # Default value for :pty is false
 # set :pty, true
-
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.1.5'
-
 # in case you want to set ruby version from the file:
 # set :rbenv_ruby, File.read('.ruby-version').strip
-
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
 
 namespace :deploy do
-
   # task :start do
   #   on roles(:all) do
   #     within "#{fetch(:deploy_to)}/current/" do
@@ -58,11 +43,22 @@ namespace :deploy do
   #     end
   #   end
   # end
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+end
+
+namespace :app do
 
   task :start do
     on roles(:all) do
       within "#{fetch(:deploy_to)}/current/" do
-        execute :rake, 'app:run'
+        execute :rake, 'l:start'
       end
     end
   end
@@ -70,17 +66,8 @@ namespace :deploy do
   task :stop do
     on roles(:all) do
       within "#{fetch(:deploy_to)}/current/" do
-        execute :rake, 'app:stop'
+        execute :rake, 'l:stop'
       end
-    end
-  end
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
     end
   end
 
