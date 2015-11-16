bb1c98822efab83546cb547eb4b26df89e3ddfb5
Vladimir <airled@mail.ru>
2015-11-16 15:35:51 +0300

    Update prices_handler.rb

--- app/workers/prices_handler.rb
+++ app/workers/prices_handler.rb
@@ -12,7 +12,7 @@ class Prices_handler
   def perform(product_url, id)
     loop do
       list = JSON.parse(Redis.new.get("ip_list"))
-      proxy_ip = list[rand(ips.size)]
+      proxy_ip = list[rand(list.size)]
       prices_url = product_url + '/prices#region=minsk&currency=byr'
       html = Nokogiri::HTML(proxy_request(prices_url, proxy_ip))
       unless (html.text.include?('503 Service Temporarily Unavailable') || html.text.include?('403 Forbidden'))
