ea548f1f9fd9f314c6feef00bde605b0ff1cd94e
airled <airled@mail.ru>
2015-11-27 12:32:24 +0300

    temporary disabled

--- lib/product_comparator.rb
+++ lib/product_comparator.rb
@@ -4,13 +4,13 @@ class Comparator
     #product is after JSON.parse 
     #params is a hash of parameters of the product fetched from a page
     params = get_product_params(product)
-    product_in_db = category.products.find_by(name: params[:name])
-    if product_in_db.nil?
+    # product_in_db = category.products.find_by(name: params[:name])
+    # if product_in_db.nil?
       product_in_db = category.products.create(params)
       # Prices_handler.perform_async(product['html_url'], product_in_db.id) if with_queue && (params[:min_price] != 'N/A')
-    else
-      check_equality(product_in_db, params)
-    end
+    # else
+    #   check_equality(product_in_db, params)
+    # end
   end
 
   private 
