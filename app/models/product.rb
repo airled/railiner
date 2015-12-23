4830c940efe2f56dbcf14c51dbc07a8c1c9edc9b
airled <airled@mail.ru>
2015-12-23 12:25:52 +0300

    css and price changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,13 @@ class Product < ActiveRecord::Base
   has_many :costs
   has_many :sellers, :through => :costs
   validates :name, presence: true
+
+  def maxprice
+    self.max_price.to_s.reverse.scan(/\d{1,3}/).join(' ').reverse.strip
+  end
+
+  def minprice
+    self.min_price.to_s.reverse.scan(/\d{1,3}/).join(' ').reverse.strip
+  end
+  
 end
