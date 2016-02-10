a10b97365cace1a7ac82c771269573afd534660f
airled <airled@mail.ru>
2016-02-10 17:56:01 +0300

    unicorn listen to localhost

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,7 +6,7 @@ working_directory '/home/railiner/current'
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
 # listen "/home/air/railiner/socket/.unicorn.sock", :backlog => 64
-listen 3000, :tcp_nopush => true
+listen '127.0.0.1:3000', :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
